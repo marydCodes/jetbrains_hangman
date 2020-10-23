621b87ed182ee734886572f85739462af78c11a1
marydCodes <40235658+marydCodes@users.noreply.github.com>
2020-10-23 09:42:16 +0000

    added to hangman

--- play_hangman.py
+++ play_hangman.py
@@ -4,7 +4,9 @@ import random
 print('''H A N G M A N
 The game will be available soon.''')
 
-rand_word()
+def play_hangman():
+    list_ = ['python', 'java', 'kotlin', 'javascript']
+    tgt_word = random.choice(list_)
 
 # intermediary practice
 def guess_sgl_word():
@@ -24,4 +26,20 @@ def rand_word():
     else:
         print("You lost!")
 
-def 
+def rand_word_hint():
+    list_ = ['python', 'java', 'kotlin', 'javascript']
+    target_word = random.choice(list_)
+    
+    tgt_len = len(target_word)
+    hint = target_word[0:3] + ("-" * (len(target_word) - 3))
+    
+    guess = str(input(f"Guess the word {hint}: "))
+    if guess == target_word:
+        print("You survived!")
+    else:
+        print("You lost!")
+
+### FUNCTION CALLS ###
+# guess_sgl_word()
+# rand_word()
+# rand_word_hint()
