ddf12d6fc3f389fb6ae01278f114402c03f61704
marydCodes <40235658+marydCodes@users.noreply.github.com>
2020-10-26 10:13:37 +0000

    moved practice functions to separate file; changed variable from w to hint in play_hangman

--- play_hangman.py
+++ play_hangman.py
@@ -15,10 +15,10 @@ def find_all(a_str, sub):
 
 # game code
 def play_hangman():
-    list_ = ['java']             # list of words to choose from
+    list_ = ['python', 'java', 'kotlin', 'javascript']
     tgt_word = random.choice(list_)     # randomly choose a word
-    w = "-" * len(tgt_word)
-    print(w)
+    hint = "-" * len(tgt_word)
+    print(hint)
 
     lives = 8
     store_guesses = []
@@ -29,21 +29,21 @@ def play_hangman():
             if guess in store_guesses:
                 print("No improvements")
                 print("\n")
-                print(w)
+                print(hint)
                 lives -= 1
             else:
-                wl = list(w)
+                hint_list = list(hint)
                 idxs = list(find_all(tgt_word, guess))
                 for i in idxs:
-                    wl[i] = guess
-                w = ''.join(wl)
+                    hint_list[i] = guess
+                hint = ''.join(hint_list)
                 print("\n")
-                print(w)
+                print(hint)
                 store_guesses.append(guess)
         else:
             print("That letter doesn't appear in the word.")
             print("\n")
-            print(w)
+            print(hint)
             lives -= 1
     
     print('''
@@ -52,39 +52,4 @@ You have no more lives left.
 Thanks for playing! 
 We'll see how well you did in the next stage''')
 
-# intermediary practice
-def guess_sgl_word():
-    guess = str(input("Guess the word: "))
-    if guess == "python":
-        print("You survived!")
-    else:
-        print("You lost!")
-
-def rand_word():
-    list_ = ['python', 'java', 'kotlin', 'javascript']
-    target_word = random.choice(list_)
-    
-    guess = str(input("Guess the word: "))
-    if guess == target_word:
-        print("You survived!")
-    else:
-        print("You lost!")
-
-def rand_word_hint():
-    list_ = ['python', 'java', 'kotlin', 'javascript']
-    target_word = random.choice(list_)
-    
-    tgt_len = len(target_word)
-    hint = target_word[0:3] + ("-" * (len(target_word) - 3))
-    
-    guess = str(input(f"Guess the word {hint}: "))
-    if guess == target_word:
-        print("You survived!")
-    else:
-        print("You lost!")
-
-### FUNCTION CALLS ###
-# guess_sgl_word()
-# rand_word()
-# rand_word_hint()
 play_hangman()
